3c55c210db257dfd3d6419e628b012c3df0f6d5b
GeorgiChochov <g.chochov@gmail.com>
2016-09-21 15:42:20 +0300

    The start of something beautiful

---
+++ src/test/java/business/TestUserModule.java
@@ -0,0 +1,18 @@
+package business;
+
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class TestUserModule {
+	
+	@Test
+	public void testRegisterUser() {
+		
+	}
+	
+	@Test
+	public void testLoginUser() {
+		
+	}
+
+}
