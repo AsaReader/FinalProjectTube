2376c91a34e774e049b526a636c75959e2055c1f
GeorgiChochov <g.chochov@gmail.com>
2016-10-04 14:31:17 +0300

    Fix character encoding of input to use UTF-8.

--- src/main/java/tube/config/WebInitializer.java
+++ src/main/java/tube/config/WebInitializer.java
@@ -1,5 +1,8 @@
 package tube.config;
 
+import javax.servlet.Filter;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
 import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
 
 import tube.web.WebConfig;
@@ -19,6 +22,13 @@ public class WebInitializer extends AbstractAnnotationConfigDispatcherServletIni
 	@Override
 	protected String[] getServletMappings() {
 		return new String[] { "/" };
+	}
+	
+	@Override
+	protected Filter[] getServletFilters() {
+		CharacterEncodingFilter characterEncodingFilter = new CharacterEncodingFilter();
+        characterEncodingFilter.setEncoding("UTF-8");
+        return new Filter[] { characterEncodingFilter};
 	}
 
 }
